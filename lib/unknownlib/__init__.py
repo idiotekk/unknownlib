e701edb45167354d4bf21e05979fe6deb317edff
idiotekk <cheziliang@gmail.com>
2023-05-28 00:47:52 -0400

    rm stats

--- lib/unknownlib/__init__.py
+++ lib/unknownlib/__init__.py
@@ -1,2 +1,2 @@
 import logging as log
-log.basicConfig(level=log.INFO, datefmt="%Y%m%d-%H:%M:%S", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+log.basicConfig(level=log.INFO, datefmt="%Y%m%d-%H:%M:%S", format='%(asctime)s - %(funcname)s - %(levelname)s - %(message)s')
