19a705e74f94fe6bb6560e66aad6cd8cfee8b398
idiotekk <cheziliang@gmail.com>
2023-06-11 14:55:21 -0400

    add more coins, update test

--- test/evm/fastw3_goerli/test.py
+++ test/evm/fastw3_goerli/test.py
@@ -31,15 +31,8 @@ class TestFastW3Methods(unittest.TestCase):
     def test_contract(self):
 
         # check balance of coin
-        token_name = "dummyCoin"
-        w3.init_contract(
-            addr="0xa2bd28f23A78Db41E49db7d7B64b6411123a8B85",
-            abi=[
-                {"constant":True,"inputs":[{"name":"account","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":False,"stateMutability":"view","type":"function"},
-                {"constant":False,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":False,"stateMutability":"nonpayable","type":"function"},
-                {"constant":True,"inputs":[{"name":"owner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":False,"stateMutability":"view","type":"function"}
-                ],
-            label=token_name)
+        token_name = "GOERLI_USDC"
+        w3.init_erc20("GOERLI_USDC")
         balance_ = w3.contract(token_name).functions.balanceOf(w3.acct.address).call()
         log.info(f"balance of {token_name} = {balance_}")
 
