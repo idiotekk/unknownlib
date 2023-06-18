a2f7178033875227191312affb2c640bd032ea47
idiotekk <cheziliang@gmail.com>
2023-06-18 00:47:08 -0400

    refac

--- examples/fastw3/event_writer.py
+++ examples/fastw3/event_writer.py
@@ -8,30 +8,31 @@ from hexbytes import HexBytes
 from unknownlib.evm.fastw3 import FastW3, Chain, log
 from unknownlib.evm.timestamp import to_int
 from unknownlib.evm.sql import SQLConnector
+from typing import Tuple, List
 
 fw = FastW3()
 
 
-def get_start_end_block_numbers(date, tz="US/Eastern"):
-    start_time = pd.to_datetime(str(date)).tz_localize(tz)
-    time_delta = pd.Timedelta("24h")
-    end_time = start_time + time_delta
-    s = fw.scan.get_block_number_by_timestamp(to_int(start_time, "s"))
-    e = fw.scan.get_block_number_by_timestamp(to_int(end_time, "s")) - 1
-    log.info(f"({start_time}, {end_time}) -> blocks({s}, {e}), {e-s+1} blocks in total")
-    return s, e 
-
-
-def fetch_one_date(date):
+def fetch_range(*,
+                contract_name: str,
+                event_names: List[str],
+                time_range: Tuple[pd.Timestamp, pd.Timestamp]) -> pd.DataFrame:
     
-    from_block, to_block = get_start_end_block_numbers(date)
-    raw_logs = fw.get_event_logs(
-        contract=contract_name,
-        event_name=event_name,
-        from_block=from_block,
-        to_block=to_block)
+    start_time, end_time = time_range
+    from_block = fw.scan.get_block_number_by_timestamp(to_int(start_time, "s"))
+    to_block = fw.scan.get_block_number_by_timestamp(to_int(end_time, "s")) - 1
+    log.info(f"({start_time}, {end_time}) -> blocks({from_block}, {to_block}), {to_block-from_block+1} blocks in total")
+
+    raw_logs = []
+    for event_name in event_names:
+        raw_logs = fw.get_event_logs(
+            contract=contract_name,
+            event_name=event_name,
+            from_block=from_block,
+            to_block=to_block)
 
     def _flatten_log(log_: dict) -> dict:
+        """ Make log a single-layer dictionary by expanding "args". """
         return {
             **{f"args_{k}": v for k, v in log_["args"].items()},
             **{k: v.hex() if isinstance(v, HexBytes) else v for k, v in log_.items() if k != "args"}
@@ -43,6 +44,21 @@ def fetch_one_date(date):
     etime = fw.get_block_time(block_number=to_block)
     df["timestamp"] = (etime - stime) / (to_block - from_block) * (df["blockNumber"] - from_block) + stime
     return df
+
+
+def fetch_one_date(*,
+                   contract_name: str,
+                   event_names: List[str],
+                   date: int,
+                   ) -> pd.DataFrame:
+
+    start_time = pd.to_datetime(str(date)).tz_localize("US/Eastern")
+    time_delta = pd.Timedelta("24h")
+    end_time = start_time + time_delta
+    return fetch_range(
+        contract_name=contract_name,
+        event_names=event_names,
+        time_range=(start_time, end_time))
 
 
 if __name__ == "__main__":
@@ -65,5 +81,5 @@ if __name__ == "__main__":
     for date in pd.date_range(str(sdate), str(edate), freq="1d"):
         date = int(date.strftime("%Y%m%d"))
         print(date)
-        df = fetch_one_date(date)
+        df = fetch_one_date(date=date, event_names=[event_name], contract_name=contract_name)
         sql.write(df, table_name=table_name, index=["blockNumber", "logIndex"])
